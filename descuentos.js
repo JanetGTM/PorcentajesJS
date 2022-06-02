2c69d489d1061fc3ffa0df4b5aba20d99e3054b4
Janet Torres <janet.gtm@gmail.com>
2022-06-01 21:03:09 -0500

    JS, HTM y CSS listos

--- descuentos.js
+++ descuentos.js
@@ -1,19 +1,31 @@
-// const precioOriginal = 120;
-// const descuento = 18;
-
 
 function calcularPrecioConDescuento(precioOriginal, descuento){
     const porcentajePrecioConDescuento = 100 - descuento;
-    const precioConDescuento = (precioOriginal * porcentajePrecioConDescuento)/100;
+    const precioConDescuento = (precioOriginal * porcentajePrecioConDescuento)/100; 
 
     return precioConDescuento;
 }
 
-// console.log({
-//     precioOriginal,
-//     descuento,
-//     porcentajePrecioConDescuento,
-//     precioConDescuento,
-// });
+function calcularAhorro(precioOriginal, descuento){
+    const savingCalc = (precioOriginal * descuento)/100;  
 
+    return savingCalc;
+}
 
+function PriceDiscount(){
+    const inputPrice = document.querySelector("#inputPrice");
+    const priceValue = inputPrice.value; 
+    const inputDiscount = document.querySelector("#inputDiscount");
+    const discountValue = inputDiscount.value; 
+    const result = document.querySelector("#result");
+    const savingResult = document.querySelector("#saving");
+    const total = calcularPrecioConDescuento(priceValue, discountValue);
+    const saving = calcularAhorro(priceValue, discountValue)
+    result.innerText =  "$" + total;
+    savingResult.innerText = "$" + saving;
+}
+
+const inputDiscount = document.querySelector("#inputDiscount");
+inputDiscount.addEventListener("input", PriceDiscount);
+const inputPrice = document.querySelector("#inputPrice");
+inputPrice.addEventListener("input", PriceDiscount);
