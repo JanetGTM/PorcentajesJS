5a9be0048699c1ad3f0fccf0f4718da4706cb28f
Janet Torres <janet.gtm@gmail.com>
2022-06-04 17:54:57 -0500

    Modificaciones finales

--- descuentos.js
+++ descuentos.js
@@ -1,15 +1,33 @@
-
 function calcularPrecioConDescuento(precioOriginal, descuento){
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precioOriginal * porcentajePrecioConDescuento)/100; 
-
     return precioConDescuento;
 }
 
 function calcularAhorro(precioOriginal, descuento){
     const savingCalc = (precioOriginal * descuento)/100;  
-
     return savingCalc;
+}
+
+function topeDescuento(){
+    const inputPrice = document.querySelector("#inputPrice");
+    const inputDiscount = document.querySelector("#inputDiscount");
+    const result = document.querySelector("#result");
+    const savingResult = document.querySelector("#saving");
+    const alert = document.querySelector("#discountAlert");
+
+    if (inputDiscount.value > 100 || inputDiscount.value < 1){
+        inputDiscount.value = "";
+        result.innerText =  "$" + inputPrice.value;
+        savingResult.innerText = "$0";
+        alert.style.display = "block";
+        alert.innerText = "Introduce un porcentaje entre 1% y 100%";
+        return inputDiscount;
+    }
+    else{
+        inputDiscount.addEventListener("input", PriceDiscount);
+        inputPrice.addEventListener("input", PriceDiscount);
+    }
 }
 
 function PriceDiscount(){
@@ -19,13 +37,15 @@ function PriceDiscount(){
     const discountValue = inputDiscount.value; 
     const result = document.querySelector("#result");
     const savingResult = document.querySelector("#saving");
+    const alert = document.querySelector("#discountAlert");
+    alert.style.display = "none";
     const total = calcularPrecioConDescuento(priceValue, discountValue);
     const saving = calcularAhorro(priceValue, discountValue)
     result.innerText =  "$" + total;
     savingResult.innerText = "$" + saving;
 }
 
-const inputDiscount = document.querySelector("#inputDiscount");
 inputDiscount.addEventListener("input", PriceDiscount);
-const inputPrice = document.querySelector("#inputPrice");
 inputPrice.addEventListener("input", PriceDiscount);
+//------------ Calcula input descuento < 100% y > 1% --------------//
+inputDiscount.addEventListener("input", topeDescuento);
