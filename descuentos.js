2b02cfafe6a6a04a29a83bce8f78bf0ffb833ad4
Janet Torres <janet.gtm@gmail.com>
2022-06-05 01:32:56 -0500

    Se valida que el campo precio tenga menos de 4 digitos

--- descuentos.js
+++ descuentos.js
@@ -1,14 +1,37 @@
+//----------- Calcula el descuento del producto
 function calcularPrecioConDescuento(precioOriginal, descuento){
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precioOriginal * porcentajePrecioConDescuento)/100; 
     return precioConDescuento;
 }
-
+//---------- Función que calcula la cantidad ahorrada
 function calcularAhorro(precioOriginal, descuento){
     const savingCalc = (precioOriginal * descuento)/100;  
     return savingCalc;
 }
+//--------- Calcula que la cantidad del precio no sea demasiado alta
+function topePrecio(){
+    const inputPrice = document.querySelector("#inputPrice");
+    const inputDiscount = document.querySelector("#inputDiscount");
+    const result = document.querySelector("#result");
+    const savingResult = document.querySelector("#saving");
+    const alert = document.querySelector("#discountAlert");
 
+    if (inputPrice.value > 99999 || inputPrice.value < 1){
+        inputPrice.value = 0;
+        savingResult.value = 0;
+        result.innerText =  "$" + inputPrice.value;
+        savingResult.innerText = "$" + savingResult.value;
+        alert.style.display = "block";
+        alert.innerText = "Introduce una cifra de máximo 5 dígitos";
+        return inputPrice;
+    }
+    else{
+        inputPrice.addEventListener("input", PriceDiscount);
+        inputDiscount.addEventListener("input", PriceDiscount);
+    }
+}
+//--------- Calcula que el descuento sea >1% y >= < 100%
 function topeDescuento(){
     const inputPrice = document.querySelector("#inputPrice");
     const inputDiscount = document.querySelector("#inputDiscount");
@@ -29,7 +52,7 @@ function topeDescuento(){
         inputPrice.addEventListener("input", PriceDiscount);
     }
 }
-
+// Función principal. Manda llamar las funciones de descuento y ahorro
 function PriceDiscount(){
     const inputPrice = document.querySelector("#inputPrice");
     const priceValue = inputPrice.value; 
@@ -44,8 +67,10 @@ function PriceDiscount(){
     result.innerText =  "$" + total;
     savingResult.innerText = "$" + saving;
 }
-
-inputDiscount.addEventListener("input", PriceDiscount);
+//------ Campo precio y descuento nos llevan a la función principal
+inputDiscount.addEventListener("input", PriceDiscount); 
 inputPrice.addEventListener("input", PriceDiscount);
-//------------ Calcula input descuento < 100% y > 1% --------------//
+//----------  Calcula que el precio sea menos a cin mil
+ inputPrice.addEventListener("input", topePrecio);
+//------------ Calcula input descuento <= 100% y > 1% --------------//
 inputDiscount.addEventListener("input", topeDescuento);
